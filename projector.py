404344abc4aca0d05ef3d025657fc990d33d4a3e
Jake Edwards <edwardsjake@live.com>
2019-03-19 13:33:41 -0500

    Got video capture working on a basic level

--- projector.py
+++ projector.py
@@ -3,3 +3,20 @@
 # Class: CSC 435-001
 # Project: SFA-Cast
 
+import numpy as np
+import cv2
+from PIL import ImageGrab
+
+#Continuously grab frames until the 'esc' key is pressed
+while True:
+    img = ImageGrab.grab(bbox=(0,0,1920,1080)) #Pillow img
+    img_np = np.array(img) #Convert pillow image to numpy array
+
+    frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB) #Convert to proper color space
+
+    cv2.imshow("Screen", frame)
+
+    if cv2.waitKey(1) ==27:
+        break
+    
+cv2.destroyAllWindows()
