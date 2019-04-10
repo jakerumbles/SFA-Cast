7a085bd4368568e2d33084cfaee3f384d3a0096c
Em <emalee.keesler@gmail.com>
2019-04-10 15:24:22 -0500

    Update SFACastGUI-CLIENT.py
    Added directory changes and creates file on desktop

--- SFACastGUI-CLIENT.py
+++ SFACastGUI-CLIENT.py
@@ -1,25 +1,39 @@
 import sys
 import os
 from tkinter import *
-import platform
 
-os = platform.system().lower()
+pathname = "~/Desktop" #Defaults path to Desktop if not changed
+
+# GUI Intialization and header
+def pathdesk():
+    pathname = "~/Desktop"
+def pathdoc():
+    pathname = "~/Documents"
+def pathpic():
+    pathname = "~/Pictures"
+def run():
+    os.system('python p_client.py')
+def opendir():
+    direct = os.path.expanduser(pathname)
+    newpath = direct + "/SFACAST-Screenshots"
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
 
 tk = Tk()  
-
 tk.title('SFA-Cast') 
-window = Canvas(tk, width=400, height=0) 
-window.pack()
-
+# Taskbar / Menu 
 menu = Menu(tk)
 tk.config(menu=menu)
-file = Menu(menu)
+file = Menu(menu) #File - Exit
 file.add_command(label="Exit", command=tk.destroy)
 menu.add_cascade(label="File", menu=file)
-edit = Menu(menu)
-edit.add_command(label="Screenshot Location")
-menu.add_cascade(label="Edit", menu=edit)
-helper = Menu(menu)
+edit = Menu(menu) #Change Screenshot path location
+edit.add_command(label="Desktop", command = pathdesk)
+edit.add_command(label="Documents", command = pathdoc)
+edit.add_command(label="Pictures", command = pathpic)
+menu.add_cascade(label="Change Screenshot Location", menu=edit)
+helper = Menu(menu) # Helper txt file open
 helper.add_command(label="Info")
 menu.add_cascade(label="Help", menu=helper)
 
@@ -33,16 +47,9 @@ img = PhotoImage(file='sfacast.png')
 pic = Label(frame, image=img)
 pic.pack()
 
-def run():
-    os.system('python p_client.py')
-    
-def opendir():
-    desktop = os.path.expanduser("~/Desktop")
-    os.startfile(desktop) 
-
 runButton = Button(tk, text='START', width=20, font =('Arial',26), fg='purple4', command=run) #Start button
 runButton.pack()
-picButton = Button(tk, text='SCREENSHOT LIBRARY',font =('Arial',26), width=20, fg='purple4',) #See Screenshots button
+picButton = Button(tk, text='SCREENSHOT',font =('Arial',26), width=20, fg='purple4',) #See Screenshots button
 picButton.pack()
 openpicButton = Button(tk, text='SCREENSHOT LIBRARY',font =('Arial',26), width=20, fg='purple4', command=opendir) #See Screenshots button
 openpicButton.pack()
