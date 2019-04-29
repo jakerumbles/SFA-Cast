238b5d8c165a96353a5eb4865fcc1c0643d62c58
Em <emalee.keesler@gmail.com>
2019-04-29 13:48:36 -0500

    again

--- newshit.py
+++ newshit.py
@@ -43,6 +43,7 @@ def main():
     SFACAST_GRP = '224.0.0.1'   # IP from Dr. Glendowne
     SFACAST_PORT = 8080         # High number port
     SFACAST_TTL = 2
+    multicast_group = (SFACAST_GROUP, SFACAST_PORT)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, SFACAST_TTL)
 
@@ -51,8 +52,8 @@ def main():
 
         while 'connected':
             #print('Client connected IP:', addr)
-            sock.sendto(str(WID).encode('utf-8'), (SFACAST_GRP, SFACAST_PORT))
-            sock.sendto(str(HGT).encode('utf-8'), (SFACAST_GRP, SFACAST_PORT))
+            sock.sendto(str(WID).encode('utf-8'), multicast_group)
+            sock.sendto(str(HGT).encode('utf-8'), multicast_group)
             thread = Thread(target=retreive_frame, args=(sock,))
             thread.start()
     finally:
