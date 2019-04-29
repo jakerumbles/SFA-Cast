139f8f30cdb839ef345709d55415708a8b9496d7
Em <emalee.keesler@gmail.com>
2019-04-29 13:51:50 -0500

    for Ruben

--- newshit2.py
+++ newshit2.py
@@ -43,10 +43,8 @@ def main():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    if IS_ALL_GROUPS:
-        sock.bind(('', SFACAST_PORT))
-    else:
-        sock.bind((SFAAST_GRP, SFACAST_PORT))
+    sock.bind(('', SFACAST_PORT))
+    #sock.bind((SFAAST_GRP, SFACAST_PORT))
     mreq = struct.pack("4sl", socket.inet_aton(SFACAST_GRP), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
