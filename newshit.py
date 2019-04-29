552d72fee146637a28cd5c91ab7366f6b586791a
Em <emalee.keesler@gmail.com>
2019-04-29 13:52:20 -0500

    for Jake

--- newshit.py
+++ newshit.py
@@ -43,7 +43,7 @@ def main():
     SFACAST_GRP = '224.0.0.1'   # IP from Dr. Glendowne
     SFACAST_PORT = 8080         # High number port
     SFACAST_TTL = 2
-    multicast_group = (SFACAST_GROUP, SFACAST_PORT)
+    multicast_group = (SFACAST_GRP, SFACAST_PORT)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, SFACAST_TTL)
 
