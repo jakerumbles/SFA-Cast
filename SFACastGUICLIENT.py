17ca41873e93061731f7125dfa55e76cfeb3c276
RubenOr <49449377+RubenOr@users.noreply.github.com>
2019-05-01 09:41:46 -0500

    Update SFACastGUICLIENT.py

--- SFACastGUICLIENT.py
+++ SFACastGUICLIENT.py
@@ -4,8 +4,10 @@ from tkinter import *
 import threading
 import platform
 
-pathname = "~/SFACAST-Screenshots" #Defaults path to Desktop if not changed
+#pathname = "~/SFACAST-Screenshots" #Defaults path to Desktop if not changed
 
+def pathname():
+        pathname = "~/SFACAST-Screenshots"
 #Gets the Operating System the computer is running
 def getOS():
         ostype = platform.system()
@@ -30,7 +32,7 @@ def readme2():
 
 def opendir():
         ostype = getOS()
-        direct = os.path.expanduser(pathname)
+        direct = os.path.expanduser(pathname())
         newpath = direct + "/SFACAST-Screenshots"
         if not os.path.exists(newpath):
                 os.makedirs(newpath)
@@ -52,45 +54,49 @@ def start_quit():
     quit_t = threading.Thread(target=quit, args=())
     quit_t.start()
 
-#Tkinter initalizaion and name bar
-tk = Tk()  
-tk.title('SFA-Cast') 
+def main():
+        #Tkinter initalizaion and name bar
+        tk = Tk()  
+        tk.title('SFA-Cast') 
 
-# Taskbar / Menu 
-menu = Menu(tk)
-tk.config(menu=menu)
-file = Menu(menu) #File - Exit
-file.add_command(label="Exit", command=tk.destroy)
-menu.add_cascade(label="File", menu=file)
-edit = Menu(menu) #Change Screenshot path location
-edit.add_command(label="Desktop", command=lambda *args: makepath("~/Desktop"))
-edit.add_command(label="Documents", command=lambda *args: makepath("~/Documents"))
-edit.add_command(label="Pictures", command=lambda *args: makepath("~/Pictures"))
-menu.add_cascade(label="Change Screenshot Location", menu=edit)
-helper = Menu(menu) # Helper txt file open
-helper.add_command(label="Info", command = readme)
-helper.add_command(label="Screenshot Help", command = readme2)
-menu.add_cascade(label="Help", menu=helper)
+        # Taskbar / Menu 
+        menu = Menu(tk)
+        tk.config(menu=menu)
+        file = Menu(menu) #File - Exit
+        file.add_command(label="Exit", command=tk.destroy)
+        menu.add_cascade(label="File", menu=file)
+        edit = Menu(menu) #Change Screenshot path location
+        edit.add_command(label="Desktop", command=lambda *args: makepath("~/Desktop"))
+        edit.add_command(label="Documents", command=lambda *args: makepath("~/Documents"))
+        edit.add_command(label="Pictures", command=lambda *args: makepath("~/Pictures"))
+        menu.add_cascade(label="Change Screenshot Location", menu=edit)
+        helper = Menu(menu) # Helper txt file open
+        helper.add_command(label="Info", command = readme)
+        helper.add_command(label="Screenshot Help", command = readme2)
+        menu.add_cascade(label="Help", menu=helper)
 
-#Label Logo
-#Label = Label(tk, text = 'SFA-Cast', font =('Arial Black',40), bg = 'purple4', fg = 'white')
-#Label.pack(pady=10,padx=10)
+        #Label Logo
+        #Label = Label(tk, text = 'SFA-Cast', font =('Arial Black',40), bg = 'purple4', fg = 'white')
+        #Label.pack(pady=10,padx=10)
 
-#Logo added
-frame = Frame(tk, width=600, height=400, background='white')
-frame.pack_propagate(0)    
-frame.pack()
-img = PhotoImage(file='sfacast.png')
-pic = Label(frame, image=img)
-pic.pack()
+        #Logo added
+        frame = Frame(tk, width=600, height=400, background='white')
+        frame.pack_propagate(0)    
+        frame.pack()
+        img = PhotoImage(file='sfacast.png')
+        pic = Label(frame, image=img)
+        pic.pack()
 
-#Buttons
-runButton = Button(tk, text='START', width=20, font =('Arial',26), fg='purple4', command=start_cast) #Start button
-runButton.pack()
-picButton = Button(tk, text='SCREENSHOT LIBRARY',font =('Arial',26), width=20, fg='purple4', command=opendir) #Take screenshots button
-picButton.pack()
-exitButton = Button(tk, text='EXIT', width=20, font =('Arial',26), fg='purple4', command=quit) #Exit button
-exitButton.pack()
+        #Buttons
+        runButton = Button(tk, text='START', width=20, font =('Arial',26), fg='purple4', command=start_cast) #Start button
+        runButton.pack()
+        picButton = Button(tk, text='SCREENSHOT LIBRARY',font =('Arial',26), width=20, fg='purple4', command=opendir) #Take screenshots button
+        picButton.pack()
+        exitButton = Button(tk, text='EXIT', width=20, font =('Arial',26), fg='purple4', command=quit) #Exit button
+        exitButton.pack()
 
-tk.mainloop()
-mainloop() 
+        tk.mainloop()
+        mainloop() 
+
+if __name__ == '__main__':
+        main()
