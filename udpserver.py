692affd37e31ea46cae721838789764779847ad4
RubenOr <49449377+RubenOr@users.noreply.github.com>
2019-05-06 16:09:27 -0500

    Update udpserver.py

--- udpserver.py
+++ udpserver.py
@@ -17,6 +17,7 @@ SFACAST_GROUP = '224.0.0.1'   # IP from Dr. Glendowne
 SFACAST_PORT = 8080         # High number port
 
 # Initalize Pygame
+# Grab current screen resolution for capture region
 pygame.init()
 infoObj = pygame.display.Info()
 WID = infoObj.current_w # Width
@@ -49,36 +50,25 @@ def retreive_frame(conn):
             min = 0
             max = 4096
             s_b = int(size_bytes.decode('utf-8'))
-            nump = int( s_b/ max)
+            nump = int( s_b / max)
             nump = nump + 1
             ending = str(nump).encode('utf-8')
             conn.sendto(ending, (SFACAST_GROUP, SFACAST_PORT))
 
-            tp = nump
-            
             check = nump
             while check != 0:
                 runs = pixels[min:max]
-                #print(len(runs))
                 conn.sendto(runs, (SFACAST_GROUP, SFACAST_PORT))
                 min = min + 4096
                 max = max + 4096
                 check -=1
-            #conn.sendto(pixels, (SFACAST_GROUP, SFACAST_PORT))
-
-def create_socket(ip, port):
-    # Socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Socket Options 
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton('144.96.33.117'))
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-    return sock
 
 def main():
+
     GRP = "224.0.0.1"
     PORT= 8080
     group = (GRP, PORT)
+    # Create socket object 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -86,10 +76,7 @@ def main():
         sock.sendto(str(WID).encode('utf-8'), group) # Send Width
         sock.sendto(str(HGT).encode('utf-8'), group) # Send Height
         while True: 
-            #thread = Thread(target=retreive_frame, args=(sock,))
             retreive_frame(sock)
-            #thread.start()
-            #time.sleep(0.2)
     finally:
         sock.close()
 
